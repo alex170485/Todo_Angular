327d7223798079ee06d3b4d5133a5656ab7ef2e7
alex170485 <alex.170485@icloud.com>
2021-05-31 19:05:46 +0300

    finish commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app-angular';
+  appTitle = 'Angular Todo Application'
 }
